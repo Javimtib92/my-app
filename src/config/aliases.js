723c278bfac95aa8d966ac9a26c2b468c6d27303
Javier <javimtib92@gmail.com>
2021-01-06 18:33:16 +0100

    Add utils aliases and service worker

--- src/config/aliases.js
+++ src/config/aliases.js
@@ -2,6 +2,7 @@ const aliases = (prefix = `src`) => ({
   '@ui': `${prefix}/ui`,
   '@config': `${prefix}/config`,
   '@api': `${prefix}/api`,
+  '@utils': `${prefix}/utils`,
 })
 
 module.exports = aliases
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import { isProduction, isServiceWorkerEnabled } from '@utils'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +13,16 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 )
+
+// If you want your app to work offline and load faster, you can change
+// REACT_APP_SERVICE_WORKER from unregister to register. Note this comes with some pitfalls.
+// using an environment variable for this is temporal, probably is better to just have this config project wise in the code.
+// Learn more about service workers: https://cra.link/PWA
+if (isProduction() && isServiceWorkerEnabled()) {
+  serviceWorkerRegistration.register()
+} else {
+  serviceWorkerRegistration.unregister()
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
